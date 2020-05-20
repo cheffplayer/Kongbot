ea7be02292985bd20c07fff5ed51b995ed9638d4
cheffplayer <dakrav51@gmail.com>
2020-05-19 23:35:45 -0700

    cannot respond to herself anymore

--- Mitsuku.py
+++ Mitsuku.py
@@ -38,56 +38,56 @@ chat = driver.find_element_by_xpath('/html/body/div[6]/table/tbody/tr/td[2]/div[
 chatroom = driver.find_element_by_xpath('/html/body/div[6]/table/tbody/tr/td[2]/div[2]/div[1]/div[2]/div/div/div/table/tbody/tr[2]/td[2]/div/div[1]/div[6]/div[10]/div[1]/div[1]/span[1]')
 chatroom.get_attribute('innerHTML')
 print("Connected to", chatroom.get_attribute('innerHTML'))
-#chat.send_keys(username, ' has connected to '+str(chatroom.get_attribute('innerHTML'))+'!')
-chat.send_keys('hello :-)')
-chat.send_keys(Keys.ENTER)
+
+wait = WebDriverWait(driver, 999999)
 
 def botrun():
     while True:
-        for a in range(6, 999999,2):
+        for a in itertools.count(6):
             b = str(a + 2)
-            wait = WebDriverWait(driver, 999999)
+            chatuser = wait.until(ec.visibility_of_element_located((By.XPATH, '/html/body/div[6]/table/tbody/tr/td[2]/div[2]/div[1]/div[2]/div/div/div/table/tbody/tr[2]/td[2]/div/div[1]/div[6]/div[10]/div[2]/div[2]/div[3]/div['+str(a)+']/p/span[2]/span'))).get_attribute('innerHTML')
+            if chatuser == username:
+                pass
+            else:
+                listen = driver.find_element_by_xpath('/html/body/div[6]/table/tbody/tr/td[2]/div[2]/div[1]/div[2]/div/div/div/table/tbody/tr[2]/td[2]/div/div[1]/div[6]/div[10]/div[2]/div[2]/div[3]/div['+str(a)+']/p/span[3]').get_attribute('innerHTML').replace('<br>', ' ')[:250]
+                print(chatuser + ':', listen)
+                driver.switch_to.window(driver.window_handles[1])
 
-            #the listen variable contains the chat input
-            listen = wait.until(ec.visibility_of_element_located((By.XPATH, '/html/body/div[6]/table/tbody/tr/td[2]/div[2]/div[1]/div[2]/div/div/div/table/tbody/tr[2]/td[2]/div/div[1]/div[6]/div[10]/div[2]/div/div[3]/div['+str(a)+']/p/span[3]'))).get_attribute('innerHTML')
-            print("Chat input: ", listen)
-            driver.switch_to.window(driver.window_handles[1])
-
-            #inputs the listen variable into the bot
-            ai.send_keys(listen)
-            ai.send_keys(Keys.ENTER)
-            time.sleep(.5)
-
-            #checks to see if the bot responded. if it didnt, then it recalibrates and outputs a preprogramed response
-            try:
-                botresponse = driver.find_element_by_xpath('/html/body/div[1]/div/div/div/div[4]/div[2]/div/div['+b+']/div/div/div').get_attribute('innerHTML').replace('<br>', ' ')[:250]
-            except:
-                ai.send_keys("|")
+                # inputs the listen variable into the bot
+                ai.send_keys(listen)
                 ai.send_keys(Keys.ENTER)
-                botresponse = "Sorry?".lower()
+                time.sleep(.5)
 
-            #removes a period if there is less than 2 of them at the end of the message
-            responselen = len(botresponse.split())
-            lastword = botresponse.split()
-            if lastword[responselen - 1][-1:] == ".":
-                if lastword[responselen - 1][-2:] != "..":
-                    botresponse = botresponse[:-1]
-            if botresponse[0:2] == ". ":
-                botresponse = botresponse[2:]
-            if botresponse[0] != "I" and botresponse[0:2] != "OK":
-                botresponse = botresponse[0].lower() + botresponse[1:]
-            for i in range(2, len(botresponse)):
-                if botresponse[i:i+2] == ". " and i < len(botresponse) - 2 and botresponse[i+2] != "I":
-                    botresponse = botresponse[0:i+2] + botresponse[i+2].lower() + botresponse[i+3:]
-            print("Chat output: ", botresponse)
-            driver.switch_to.window(driver.window_handles[0])
+                # checks to see if the bot responded. if it didnt, then it recalibrates and outputs a preprogramed response
+                try:
+                    botresponse = driver.find_element_by_xpath('/html/body/div[1]/div/div/div/div[4]/div[2]/div/div[' + b + ']/div/div/div').get_attribute('innerHTML').replace('<br>', ' ')[:250]
+                except:
+                    ai.send_keys("|")
+                    ai.send_keys(Keys.ENTER)
+                    botresponse = "Sorry?".lower()
 
-            #delays the response based on character length
-            delayseconds = 2 + len(botresponse) / 8
-            time.sleep(delayseconds)
+                # removes a period if there is less than 2 of them at the end of the message
+                responselen = len(botresponse.split())
+                lastword = botresponse.split()
+                if lastword[responselen - 1][-1:] == ".":
+                    if lastword[responselen - 1][-2:] != "..":
+                        botresponse = botresponse[:-1]
+                if botresponse[0:2] == ". ":
+                    botresponse = botresponse[2:]
+                if botresponse[0] != "I" and botresponse[0:2] != "OK":
+                    botresponse = botresponse[0].lower() + botresponse[1:]
+                for i in range(2, len(botresponse)):
+                    if botresponse[i:i + 2] == ". " and i < len(botresponse) - 2 and botresponse[i + 2] != "I":
+                        botresponse = botresponse[0:i + 2] + botresponse[i + 2].lower() + botresponse[i + 3:]
+                print("Chat output: ", botresponse)
+                driver.switch_to.window(driver.window_handles[0])
 
-            #sends message to chat
-            chat.send_keys(botresponse)
-            chat.send_keys(Keys.ENTER)
-            print()
+                # delays the response based on character length
+                delayseconds = 2 + len(botresponse) / 8
+                time.sleep(delayseconds)
+
+                # sends message to chat
+                chat.send_keys(botresponse)
+                chat.send_keys(Keys.ENTER)
+                print()
 botrun()
