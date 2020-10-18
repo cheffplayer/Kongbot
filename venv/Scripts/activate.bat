abbdcac9594a78455085f4119beffa3c3142f7f2
cheffplayer <dakrav51@gmail.com>
2020-10-17 21:00:35 -0700

    ported to python 3.9

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Downloads\Trading\Program\trading\venv
+set VIRTUAL_ENV=D:\Documents\GitHub\Mitsuku\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
